72acf1b6422f6409caeaec86612f57fc65bc3415
ohwk123 <seojeong@sookmyung.ac.kr>
2022-10-04 16:21:14 +0900

    01

--- main.c
+++ main.c
@@ -7,12 +7,10 @@ int main(int argc, char *argv[]) {
 	int x;
 	printf("정수 하나를 입력하시오:");
 	scanf("%i", &x);
-	if (x>0)
-	 printf("양수입니다");
-	else if (x==0)
-  	 printf("0입니다."); 
+	if (x>=0)
+	 printf("절대값은 %i입니다", x);
+
 	else
-	 printf("음수입니다");
-	
+	 printf("절대값은 %i입니다", -x);
 	return 0;
 }
