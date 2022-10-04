394a878bd4c7318d2d062883f6ee2954ec0640b0
ohwk123 <seojeong@sookmyung.ac.kr>
2022-10-04 17:32:30 +0900

    04

--- main.c
+++ main.c
@@ -4,16 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int cnt=0;
-	printf("input a string:");
+	int sum=0;
+	int x;
+	int i;
+	printf("정수를 입력하시오:");
+	scanf("%d", &x);
+	for(i=0; i<=x;i++)
+	 sum = sum + i;
 	
-	while ((c=getchar())!='\n')//입력 문자가 갱행문자가 나올때까지 반복 
-	{if(c>=48 && c<=57)
-	  cnt+=1;
-	else
-	  continue;
-	 }
-	 printf("the number of digits is %i", cnt);
+	printf("더하기 결과는 %i입니다", sum);
 	return 0;
 }
