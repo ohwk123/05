4b38f1617842909cc1f7a79d46b54c5e82dcabe0
ohwk123 <seojeong@sookmyung.ac.kr>
2022-10-04 18:11:57 +0900

    02

--- main.c
+++ main.c
@@ -4,29 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int answer=12;
 	int x;
-	int sum=1;
-	
-	do
-{
-    printf("숫자를 입력하시오:");
-	scanf("%d", &x);
-	if (x>answer)
-	  printf("정답보다 높습니다");
-	  
-	else if (x<answer) 
-	  printf("정답보다 낮습니다");
-	else
-	  break;
-	sum += 1;
-	  
-}	while (x != answer);
-   
+	printf("정수 하나를 입력하시오:");
+	scanf("%i", &x);
+	if (x>=0)
+	 printf("절대값은 %i입니다", x);
+	else 
+	printf("절대값은 %i입니다", -x);
 
-    printf("정답입니다!, 시도 횟수%d", sum);
-	  
-	
 	
 	return 0;
 }
