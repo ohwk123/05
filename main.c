2c52118efa79de0b95e86cab38ab49b2be958609
ohwk123 <seojeong@sookmyung.ac.kr>
2022-10-04 18:08:20 +0900

    06

--- main.c
+++ main.c
@@ -4,23 +4,29 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x,y;
-	char op;
-	int result;
+	int answer=12;
+	int x;
+	int sum=1;
 	
-	printf("enter the calculation:");
-	scanf("%d %c %d", &x, &op, &y);
-	
-	if (op == '+')
-	 result = x + y;
-	else if (op == '-')
-	 result = x - y;
-	else if (op =='*')
-	 result = x*y;
+	do
+{
+    printf("숫자를 입력하시오:");
+	scanf("%d", &x);
+	if (x>answer)
+	  printf("정답보다 높습니다");
+	  
+	else if (x<answer) 
+	  printf("정답보다 낮습니다");
 	else
-	 result = x/y;
-	 
-	 
-    printf("= %i\n", result);
+	  break;
+	sum += 1;
+	  
+}	while (x != answer);
+   
+
+    printf("정답입니다!, 시도 횟수%d", sum);
+	  
+	
+	
 	return 0;
 }
